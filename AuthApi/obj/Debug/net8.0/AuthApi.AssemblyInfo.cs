8cf94f4e240a3dfd07fba3202d61c3e6c22aa5bf
PoznanaKing <pozsgain@kkszki.hu>
2025-02-10 08:58:05 +0100

    The post controller endpoints are repaired. Image upload is already possible now via swagger. the post comment controller is ready to use.

--- AuthApi/obj/Debug/net8.0/AuthApi.AssemblyInfo.cs
+++ AuthApi/obj/Debug/net8.0/AuthApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+024f47db1719bfc61f8cc8e13cdc33dc5c605510")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+753f748e0cc8e43f87fbce14150567cf6eed494d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
